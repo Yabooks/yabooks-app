c1923b5ad5cf615fdf6ae5062a4c84b1e61d3ed5
Leopold Mathias Regner <leopold.mathias.regner@gmail.com>
2025-10-26 11:11:30 +0100

    tax code selector

--- public/tax-code-selector.js
+++ public/tax-code-selector.js
@@ -1,21 +1,20 @@
 if(typeof module === "object")
     SearchableDropdown = require("./searchable-dropdown").SearchableDropdown;
 
+/**
+ * <tax-code-selector :tax_codes="tax_codes"
+ *     v-model:tax_code="tx.tax_code" v-model:tax_code_base="tx.tax_code_base"
+ *     v-model:tax_sub_code="tx.tax_sub_code" v-model:tax_sub_code_base="tx.tax_sub_code_base"
+ *     v-model:tax_percent="tx.tax_percent" />
+ */
 const TaxCodeSelector = (
 {
     template: `
         <span>
             <input type="checkbox" v-model="isTaxBase" @change="visualTaxCodeSelected" title="is tax base" />
-            <select v-model="selectedVisualTaxCode" @change="visualTaxCodeSelected" style="max-width: 200px">
-                <option :value="null">
-                    no tax
-                </option>
-                <template v-for="visualTaxCode in visualTaxCodes" :key="visualTaxCode.displayName">
-                    <option :value="visualTaxCode">
-                        {{ visualTaxCode.displayName }}
-                    </option>
-                </template>
-            </select>
+            <searchable-dropdown
+                :options="visualTaxCodes" label="displayName" value="value"
+                v-model:selected="selectedTaxCode" @change="visualTaxCodeSelected">
         </span>
     `,
 
@@ -23,28 +22,28 @@ const TaxCodeSelector = (
 
     props:
     {
-        tax_code: {
+        tax_code: { // LedgerTransaction.tax_code
             type: String,
             required: true
         },
-        tax_code_base: {
-            type: String,
-            required: true
-        },
-        tax_sub_code: {
-            type: String,
-            required: true
-        },
-        tax_sub_code_base: {
-            type: String,
-            required: true
-        },
-        tax_percent: {
-            type: Number,
-            required: true
-        },
-        tax_codes: {
+        tax_codes: { // list of available tax codes as of GET /api/v1/tax-codes
             type: Array,
+            required: true
+        },
+        tax_code_base: { // v-model:tax_code_base for LedgerTransaction.tax_code_base
+            type: String,
+            required: true
+        },
+        tax_sub_code: { // v-model:tax_sub_code for LedgerTransaction.tax_sub_code
+            type: String,
+            required: true
+        },
+        tax_sub_code_base: { // v-model:tax_sub_code_base for LedgerTransaction.tax_sub_code_base
+            type: String,
+            required: true
+        },
+        tax_percent: { // v-model:tax_percent for LedgerTransaction.tax_percent
+            type: Number,
             required: true
         }
     },
@@ -53,8 +52,8 @@ const TaxCodeSelector = (
     {
         return {
             isTaxBase: false,
-            visualTaxCodes: [],
-            selectedVisualTaxCode: null,
+            visualTaxCodes: [ { displayName: "no tax", value: null } ],
+            selectedTaxCode: null,
         };
     },
 
@@ -78,21 +77,24 @@ const TaxCodeSelector = (
                     {
                         this.visualTaxCodes.push(
                         {
-                            percent: rate,
-                            code: taxCode.code,
-                            sub_code: subCode.code,
+                            value: {
+                                percent: rate,
+                                code: taxCode.code,
+                                sub_code: subCode.code,
+                            },
                             displayName: `${filters.formatTaxCode(taxCode.code)} ${taxCode.description ?? ""} ${subCode.description ?? ""} ${rate ? `${rate}%` : ""}`.trim()
                         });
 
+                        // mark currently set value as selected
                         if(this.tax_code == taxCode.code && this.tax_sub_code == subCode.code && this.tax_percent == rate)
                         {
                             this.isTaxBase = false;
-                            this.selectedVisualTaxCode = this.visualTaxCodes[this.visualTaxCodes.length - 1];
+                            this.selectedTaxCode = this.visualTaxCodes[this.visualTaxCodes.length - 1].value;
                         }
                         else if(this.tax_code_base == taxCode.code && this.tax_sub_code_base == subCode.code && this.tax_percent == rate)
                         {
                             this.isTaxBase = true;
-                            this.selectedVisualTaxCode = this.visualTaxCodes[this.visualTaxCodes.length - 1];
+                            this.selectedTaxCode = this.visualTaxCodes[this.visualTaxCodes.length - 1].value;
                         }
                     }
             this.$forceUpdate();
@@ -100,11 +102,12 @@ const TaxCodeSelector = (
 
         visualTaxCodeSelected()
         {
-            this.$emit("update:tax_code", this.isTaxBase ? null : this.selectedVisualTaxCode.code);
-            this.$emit("update:tax_code_base", this.isTaxBase ? null : this.selectedVisualTaxCode.sub_code);
-            this.$emit("update:tax_sub_code", this.isTaxBase ? this.selectedVisualTaxCode.code : null);
-            this.$emit("update:tax_sub_code_base", this.isTaxBase ? this.selectedVisualTaxCode.sub_code : null);
-            this.$emit("update:tax_percent", this.selectedVisualTaxCode.rate);
+            this.$emit("update:tax_code", this.isTaxBase ? null : this.selectedTaxCode.code);
+            this.$emit("update:tax_code_base", this.isTaxBase ? this.selectedTaxCode.code : null);
+            this.$emit("update:tax_sub_code", this.isTaxBase ? null : this.selectedTaxCode.sub_code);
+            this.$emit("update:tax_sub_code_base", this.isTaxBase ? this.selectedTaxCode.sub_code : null);
+            this.$emit("update:tax_percent",this.selectedTaxCode.percent);
+            this.$emit("change"); // fire @change
         }
     }
 });
